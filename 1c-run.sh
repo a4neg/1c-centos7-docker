75fed4dd125ec463549cd43f902d2bace02c369d
a4neg <datch2003@mail.ru>
2017-11-06 19:59:12 +0300

    added --volume /var/log/1C:/var/log/1C \  #mount logs to host machine

--- 1c-run.sh
+++ 1c-run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-docker create --name=1c_server_data -v /var/log/1c -v /home/usr1cv8/ a4neg/1c-centos7-docker
+docker create --name=1c_server_data -v /var/log/1C -v /home/usr1cv8/ a4neg/1c-centos7-docker
 docker run --name=1c_server --restart=always \
  --net host \
  --detach \
  --volumes-from 1c_server_data  \
  --volume /etc/localtime:/etc/localtime:ro \
+ --volume /var/log/1C:/var/log/1C \
  -h termit.srv \
  a4neg/1c-centos7-docker
 
